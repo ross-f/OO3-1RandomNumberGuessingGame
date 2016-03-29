0ccca3edb326d3070ea488ca61073b7277d99044
Ross <rssfltchr@gmail.com>
2016-03-29 20:31:04 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Random r = new Random();
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int generatedNumber = random.nextInt(100);
 
+        while
+
+
+
+        System.out.println(generatedNumber);
     }
 }
